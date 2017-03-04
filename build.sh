a291408aa725060004571a83063b27a38f3dfa64
yage99 <zhangya998@gmail.com>
2017-03-04 11:07:36 +0000

    fix some bugs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm lib/libDHT.so
 rm /usr/lib/libDHT.so
-gcc -o lib/libDHT.so -shared com_yage_raspberry_DHT_jni.c -lwiringPi -I/usr/lib/jvm/java-7-openjdk-armhf/include DHT.c
+gcc -o lib/libDHT.so -shared com_yage_raspberry_DHT_jni.c -lwiringPi -I/usr/lib/jvm/java-8-openjdk-armhf/include -I/usr/lib/jvm/java-8-openjdk-armhf/include/linux DHT.c
 ln lib/libDHT.so /usr/lib
 ldconfig
